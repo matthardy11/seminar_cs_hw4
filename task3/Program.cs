4c425c47060ac0adce5a0fcd4a2ef84dba5b774b
matthardy11 <matthardy2560@gmail.com>
2023-01-26 21:40:25 +0400

    comlete task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] FillArray()
+{
+  Random rand = new Random();
+  int[] array = new int[8];
+  for(int i = 0; i < array.Length; i++)
+  {
+    array[i] = rand.Next(0, 101);
+  }
+  return array;
+}
+
+void ShowArray(int[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+  {
+    System.Console.Write(array[i] + " ");
+  }
+}
+
+ShowArray(FillArray());
