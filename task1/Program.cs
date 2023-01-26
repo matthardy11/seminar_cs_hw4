f0b522b36845a3d8f0e034a861622b852f3ef7a9
matthardy11 <matthardy2560@gmail.com>
2023-01-26 21:00:25 +0400

    comlete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int InputNumber(string str)
+{
+  int number;
+  string text;
+  while(true)
+  {
+    Console.WriteLine(str);
+    text = Console.ReadLine();
+    if(int.TryParse(text, out number))
+    {
+      break;
+    }
+    System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз.");
+  }
+  return number;
+}
+
+int DegreeOfNumber(int a, int b)
+{
+int result = a;
+for(int i = 1; i < b; i++)
+  {
+    result = result*a;
+  }
+return result;
+}
+
+int num1 = InputNumber("Введите число А: ");
+int num2 = InputNumber("Введите число B: ");
+
+System.Console.WriteLine("Число " + num1 + " в степени " + num2 + " = " + DegreeOfNumber(num1, num2));
